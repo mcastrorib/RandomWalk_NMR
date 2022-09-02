87676ff1433dc1147f7e55d7b4dafd0b32c4beb7
Matheus Ribeiro <matheusdcribeiro@gmail.com>
2022-09-02 11:00:09 -0300

    modified cmake build script, now suporting flag to distinguish release and debug modes, removed old code

--- install_rwnmr.sh
+++ install_rwnmr.sh
@@ -45,25 +45,3 @@ then
 	echo "Creating symbolic link to executable RWNMR"
 	ln -sf ${TARGET_DIR}/RWNMR RWNMR
 fi
-
-
-# ## BUILDING RELEASE VERSION
-# # Build directories path
-# RELEASE_DIR='./build/release'
-# # Configure the build
-# cmake -S ${SRC_DIR} -B ${RELEASE_DIR} -D CMAKE_BUILD_TYPE=Release
-# # Actually build the binaries
-# cmake --build build/release -j${procs}
-# # Create symbolic link in root directory
-# ln -sf ${RELEASE_DIR}/RWNMR RWNMR
-
-# ## BUILDING DEBUG VERSION
-# # Build directories path
-# DEBUG_DIR='./build/debug'
-# # Configure the build
-# cmake -S ${SRC_DIR} -B ${DEBUG_DIR} -D CMAKE_BUILD_TYPE=Debug
-# # Actually build the binaries
-# cmake --build build/debug -j${procs}
-
-
-
