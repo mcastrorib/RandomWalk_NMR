76fe7f232fb8ff7021ab40664a609d7412fcb480
Matheus Ribeiro <matheusdcribeiro@gmail.com>
2022-09-02 10:59:17 -0300

    modified cmake build script, now suporting flag to distinguish release and debug modes

--- install_rwnmr.sh
+++ install_rwnmr.sh
@@ -5,28 +5,65 @@
 
 # Get system available processors
 procs=$(nproc --all)
-echo "Building RWNMR with ${procs} processors..."
 
 # Source code path
 SRC_DIR='./src/backend'
 
-## BUILDING RELEASE VERSION
-# Build directories path
-RELEASE_DIR='./build/release'
-# Configure the build
-cmake -S ${SRC_DIR} -B ${RELEASE_DIR} -D CMAKE_BUILD_TYPE=Release
-# Actually build the binaries
-cmake --build build/release -j${procs}
-# Create symbolic link in root directory
-ln -sf ${RELEASE_DIR}/RWNMR RWNMR
+# Assign compilation mode from command line args
+TARGET_MODE="Release"
+TARGET_DIR="./build/release"
 
-## BUILDING DEBUG VERSION
-# Build directories path
-DEBUG_DIR='./build/debug'
+for i in "$@"; do
+	case $1 in
+		-r|--release)
+			TARGET_MODE="Release"
+			TARGET_DIR="./build/release"
+			shift
+			;;
+		-d|--debug)
+			TARGET_MODE="Debug"
+			TARGET_DIR="./build/debug"
+			shift
+			;;
+		*)
+			TARGET_MODE="Release"
+			TARGET_DIR="./build/release"
+			;;
+	esac
+done
+
+## BUILDING RWNMR
+echo "Building RWNMR (${TARGET_MODE}) with ${procs} processors..."
 # Configure the build
-cmake -S ${SRC_DIR} -B ${DEBUG_DIR} -D CMAKE_BUILD_TYPE=Debug
+cmake -S ${SRC_DIR} -B ${TARGET_DIR} -D CMAKE_BUILD_TYPE=${TARGET_MODE}
 # Actually build the binaries
-cmake --build build/debug -j${procs}
+cmake --build ${TARGET_DIR} -j${procs}
+
+if [ ${TARGET_MODE} == "Release" ] 
+then
+	# Create symbolic link in root directory
+	echo "Creating symbolic link to executable RWNMR"
+	ln -sf ${TARGET_DIR}/RWNMR RWNMR
+fi
+
+
+# ## BUILDING RELEASE VERSION
+# # Build directories path
+# RELEASE_DIR='./build/release'
+# # Configure the build
+# cmake -S ${SRC_DIR} -B ${RELEASE_DIR} -D CMAKE_BUILD_TYPE=Release
+# # Actually build the binaries
+# cmake --build build/release -j${procs}
+# # Create symbolic link in root directory
+# ln -sf ${RELEASE_DIR}/RWNMR RWNMR
+
+# ## BUILDING DEBUG VERSION
+# # Build directories path
+# DEBUG_DIR='./build/debug'
+# # Configure the build
+# cmake -S ${SRC_DIR} -B ${DEBUG_DIR} -D CMAKE_BUILD_TYPE=Debug
+# # Actually build the binaries
+# cmake --build build/debug -j${procs}
 
 
 
